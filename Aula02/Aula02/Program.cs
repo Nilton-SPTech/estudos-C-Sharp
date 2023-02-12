c5e59b17eb1f9df5b8ce867b19ac90897595c9b0
Nilton-Izio <nilton.gomes@izio.com.br>
2023-02-12 19:50:58 -0300

    Adicionando os repositórios de estudos do curso

---
+++ Aula02/Aula02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aula02{
+    class Program{
+        static void Main(string[] args) {
+
+            /*
+             Tipos de var numéricas
+              byte 
+              int
+              long
+              floatd
+              double 
+              decimal 
+            
+            Tipo booleano 
+              bool
+             
+            Guardar o carctarer unitário 
+              char = 'V'
+             */
+
+            
+        }
+    }
+}

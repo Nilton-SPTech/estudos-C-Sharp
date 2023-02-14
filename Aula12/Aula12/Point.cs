555ece3aef676bb1948b995e9faec6404b114f97
Nilton-Izio <nilton.gomes@izio.com.br>
2023-02-14 14:27:33 -0300

    Nullable e suas funções - Operador de qualicencia nula

---
+++ Aula12/Aula12/Point.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Aula12
+{
+    //STRUCT É UM TIPO VALOR, SEMELHANTE A VAR
+    struct Point
+    {
+        public int X; 
+        public int Y;
+        public override string ToString()
+        {
+            return "("+ X + "," + Y +")";
+        }
+
+
+    }
+}

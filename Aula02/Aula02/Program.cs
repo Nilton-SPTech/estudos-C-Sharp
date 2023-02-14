448599dfe531ce9a5a2b69dc4fa1efce5f5998ea
Nilton-Izio <nilton.gomes@izio.com.br>
2023-02-14 15:33:50 -0300

    Teste gitignore oficial

---
+++ Aula02/Aula02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aula02{
+    class Program{
+        static void Main(string[] args) {
+
+            /*
+             Tipos de var numéricas
+              byte 
+              int
+              long
+              floatd
+              double 
+              decimal 
+            
+            Tipo booleano 
+              bool
+             
+            Guardar o carctarer unitário 
+              char = 'V'
+
+            
+             */
+
+            int min = int.MinValue;
+            decimal max = decimal.MaxValue;
+            Console.WriteLine(max);
+        }
+    }
+}

ecfbbf0755785070e187bde1affa65d5f1ff9b98
Nilton-Izio <nilton.gomes@izio.com.br>
2023-02-17 14:15:47 -0300

    Adicionando métodos extensions

---
+++ Aula34/Aula34/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Aula34
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //EXTESIONS
+            //FUNCIONA COMO UM MÉTODO PERSONALIZADO PARA CLASSES NATIVAS DO C#
+            //PODEMOS CRIAR NOVAS VARIANTES OU MÉTODOS PARA AS CLASSES
+
+            string s1 = "Nossa, Extensions é muito MANEIRO!!!";
+
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
